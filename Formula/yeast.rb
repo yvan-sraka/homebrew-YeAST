7f09ba0612e6f23f3b015d186bf4103bb3a7a689
Yvan Sraka <yvan@sraka.pw>
2018-12-28 16:58:07 +0100

    v0.10.0

--- Formula/yeast.rb
+++ Formula/yeast.rb
@@ -1,11 +1,12 @@
 class Yeast < Formula
   desc "Yet Another Shell Trick"
   homepage "https://github.com/yvan-sraka/YeAST"
-  url "https://github.com/yvan-sraka/YeAST/archive/v0.9.0.tar.gz"
-  sha256 "6c146f27bc41eb5279caf4274672214566327b8d79fef477c322779fe3211fde"
+  url "https://github.com/yvan-sraka/YeAST/archive/v0.10.0.tar.gz"
+  sha256 "c90fc0ac4258c0bff945de566d230cef1c1b64ff14fc79202d490eebceefbe01"
   depends_on "rust" => :build
 
   def install
     system "cargo", "install", "--root", prefix, "--path", "."
+    bin.install "kombucha"
   end
 end
