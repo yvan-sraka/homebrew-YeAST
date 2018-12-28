a433c14f8ea5192d12645a7bdbb31c839f823d8c
Yvan Sraka <yvan@sraka.pw>
2018-12-28 16:54:30 +0100

    v0.5.0

--- Formula/yeast.rb
+++ Formula/yeast.rb
@@ -1,8 +1,8 @@
 class Yeast < Formula
   desc "Yet Another Shell Trick"
   homepage "https://github.com/yvan-sraka/YeAST"
-  url "https://github.com/yvan-sraka/YeAST/archive/v0.4.0.tar.gz"
-  sha256 "12cb4d845e1c22c565b4d8291d5a758e83b41081c1c0cf777f57008cad66fff4"
+  url "https://github.com/yvan-sraka/YeAST/archive/v0.5.0.tar.gz"
+  sha256 "2833466d2277ebc92e90b8755fbcdc7c0a4ec61dba6d461537bfe8763a1760ca"
   depends_on "rust" => :build
 
   def install
